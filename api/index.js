46ac8fc3b3a53f85d4140efc88a0715007381992
AYUSH SISODIYA <92314240+Asrhacker765@users.noreply.github.com>
2024-08-19 10:18:28 +0530

    update

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const app = express();
 const dotenv = require("dotenv");
 const products = require("./data/product")
 dotenv.config();
+const cors = require("cors")
 const PORT = process.env.PORT;
 
 const mongoose =require("mongoose")
@@ -16,20 +17,25 @@ mongoose.connect(process.env.MONGOOSEDB_RUL).then(()=>console.log("db connected"
 
 const databaseseeder= require('./databaseseeder')
 const userRoute = require("./routes/User");
-app.use(express.json())
 const productRoute = require("./routes/Product");
 const orderRoute = require("./routes/Order");
 //database seeder routes
+app.use(express.json())
+app.use(cors())
 app.use('/api/seed',databaseseeder)
 //routes for users
 app.use("/api/users", userRoute);
-app.use(express.json())
+
 //routes for products
 app.use("/api/products", productRoute);
 
 //routes for orders
 app.use("/api/orders", orderRoute);
 
+
+app.use("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
 
 app.listen(PORT|| 9000, () => {
     console.log(`Server listening on port ${PORT}`);
--- client/src/Redux/Constants/BASE_URL.js
+++ client/src/Redux/Constants/BASE_URL.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:4005"
+export const BASE_URL = "http://localhost:4008"
 //export const BASE_URL = "https://node-react-shop-pearl.vercel.app"

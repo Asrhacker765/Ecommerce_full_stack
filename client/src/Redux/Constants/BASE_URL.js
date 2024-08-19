8cd204aa9b0d3c2e304574d3b4007412e6ded40b
AYUSH SISODIYA <92314240+Asrhacker765@users.noreply.github.com>
2024-08-19 11:56:13 +0530

    update url

--- client/src/Redux/Constants/BASE_URL.js
+++ client/src/Redux/Constants/BASE_URL.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:4008"
-//export const BASE_URL = "https://node-react-shop-pearl.vercel.app"
+//export const BASE_URL = "http://localhost:4008"
+export const BASE_URL = "https://ecommerce-full-stack-6ewz.vercel.app/"

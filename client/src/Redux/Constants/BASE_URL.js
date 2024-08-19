32941e0469a57246885abc3a289d5dff84e982ab
AYUSH SISODIYA <92314240+Asrhacker765@users.noreply.github.com>
2024-08-19 12:07:54 +0530

    up

--- client/src/Redux/Constants/BASE_URL.js
+++ client/src/Redux/Constants/BASE_URL.js
@@ -1,2 +1,2 @@
 //export const BASE_URL = "http://localhost:4008"
-export const BASE_URL = "https://ecommerce-full-stack-6ewz-nngqgd3am-asrs-projects-517b3b63.vercel.app/"
+export const BASE_URL = "https://ecommerce-full-stack-6ewz.vercel.app/"
